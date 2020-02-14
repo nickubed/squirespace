3bcf717efb0b3c6e6ee9316b5b23480ed1ec55d3
Gavin Callander <gavincallander@Gavins-MacBook-Pro.local>
2020-02-14 12:11:25 -0800

    removing useless modules

--- src/content/pages/Search.jsx
+++ src/content/pages/Search.jsx
@@ -1,24 +1,19 @@
 // dependencies
 import React, { useEffect, useState } from 'react';
 // components
-import Cards, { Card } from 'react-swipe-card';
+
 // test data
 import { testData } from '../data/cardTestData';
 
 export const Search = () => {
 
+    const handlers = useSwipeable({ onSwiped: (eventData) => eventHandler, ...config });
+
+
     return (
         <div className='page search'>
-            <div className='search-container'>
-                <Cards onEnd={action('end')} className='cards'>
-                    {testData.map(item =>
-                        <Card
-                            onSwipeLeft={action('swipe left')}    
-                            onSwipeRight={action('swipe right')}>
-                                <h2>{item.name}</h2>
-                        </Card>
-                    )}
-                </Cards>
+            <div className='search-container' {...handlers}>
+                You can swipe here
             </div>
         </div>
     )
