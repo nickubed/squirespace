8c70fcb3d09e81318c740ab57860853fbbbb39c7
Gavin Callander <gavincallander@Gavins-MacBook-Pro.local>
2020-02-14 10:18:03 -0800

    updated changes prior to auth routes being written

--- src/content/App.js
+++ src/content/App.js
@@ -1,5 +1,6 @@
 // dependencies
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 // pages
 import { Home, Landing, SignIn, SignUp } from './pages';
 import { Footer, Header } from './components';
@@ -10,23 +11,32 @@ import './components/style.scss';
 
 export const App = () => {
 
-    const [user, setUser] = useState();
+    const [user, setUser] = useState(null);
 
     let content;
     if (user) {
+        content = ( 
+            <>
+                <Route exact path='/' component={Landing} />
+                <Route path='/signin' component={SignIn} />
+                <Route path='/signup' component={SignUp} />
+            </>
+        )
+    } else {
         content = (
-            
+            <>
+                <Header />
+                <Route path='/' component={Home} />
+                <Footer />
+            </>
         )
     }
 
     return (
         <div className='app'>
-            {/* <Landing /> */}
-            {/* <SignIn /> */}
-            {/* <SignUp /> */}
-            <Header />
-            <Home />
-            <Footer />
+            <Router>
+                {content}
+            </Router>
         </div>
     )
 };
--- src/content/pages/Home.jsx
+++ src/content/pages/Home.jsx
@@ -5,20 +5,17 @@ export const Home = () => {
     return (
         <div className='page home'>
             <div className='home-top'>
-                <img className='profile-img' src='https://vignette.wikia.nocookie.net/heroes-of-camelot/images/7/79/Squire.jpg/revision/latest?cb=20140202173246' alt='image of user' />
-                <h3>User</h3>
+                <img className='home-top-img' src='https://www.placecage.com/200/300' alt='home pic' />
+                <div className='home-top-stats'>
+                    <h2>Name</h2>
+                    <h3>Age</h3>
+                </div>
             </div>
-            <hr className='home-hr' />
-            <div className='home-main'>
-                <h4>Bio</h4>
-                <p>Just returned from crusade. Lost my master to an arrow to the knee.</p>
-                <p>Not sure what I'm looking for, but companionship and glory are always welcome.</p>
-            </div>
-            <hr className='home-hr' />
-            <div className='home-bottom'>
-                <h4>Preferences</h4>
-                <p>Squires</p>
-                <p>Knights</p>
+            <div className='home-bot'>
+                <div className='home-bio-container'>
+                    <h2>Bio</h2>    
+                    <h4>Looking for love, life and indentured servitude.</h4>
+                </div>
             </div>
         </div>
     )
