dc41c1369966b16297ccee6b7288796c53fc5d5e
brandiw <brandimichelle.0911@gmail.com>
2020-02-14 11:08:14 -0800

    Signin page up

--- src/content/App.js
+++ src/content/App.js
@@ -1,6 +1,7 @@
 // dependencies
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 // pages
 import { Home, Landing, SignIn, SignUp } from './pages';
 import { Footer, Header } from './components';
@@ -13,21 +14,71 @@ export const App = () => {
 
     const [user, setUser] = useState(null);
 
+    // Define any onload actions (e.g., to look for the token)
+    useEffect(() => {
+        console.log('check for token!')
+        decodeToken();
+    }, [])
+
+    // Helper function to update the user (login, signup, and logout use this)
+    const updateUser = newToken => {
+        if (newToken) {
+            // Save the token 
+            localStorage.setItem('userToken', newToken);
+
+            // Update the user with the token's info
+            decodeToken(newToken);
+        }
+        else {
+            setUser(null);
+        }
+    }
+
+    // Helper function to decode existing tokens
+    const decodeToken = existingToken => {
+        let token = existingToken || localStorage.getItem('userToken');
+        console.log('The token is:', token);
+
+        // Decode token
+        if (token) {
+            let decoded = jwtDecode(token);
+
+            // If the token is not decoded or it is expired, NO USER!
+            if (!decoded || Date.now() > decoded.exp * 1000) {
+                console.log('Expired or bad token?');
+                setUser(null);
+            }
+            else {
+                // This is the user data - YAY
+                console.log('YAY! Good token!');
+                setUser(decoded);
+            }
+        } 
+        else {
+            setUser(null);
+        }
+    }
+
     let content;
     if (user) {
         content = ( 
             <>
                 <Route exact path='/' component={Landing} />
-                <Route path='/signin' component={SignIn} />
-                <Route path='/signup' component={SignUp} />
+                <Route path='/signin' render={() => <SignIn updateUser={updateUser} user={user} />} />
+                <Route path='/signup' render={() => (
+                    <SignUp updateUser={updateUser} user={user} />
+                )} />
+                <Route path="/profile" component={Landing} />
             </>
         )
     } else {
         content = (
             <>
-                <Header />
-                <Route path='/' component={Home} />
-                <Footer />
+                <Route exact path='/' component={Home} />
+                <Route path='/signin' render={() => <SignIn updateUser={updateUser} user={user} />} />
+                <Route path='/signup' render={() => (
+                    <SignUp updateUser={updateUser} user={user} />
+                )} />
             </>
         )
     }
@@ -35,7 +86,9 @@ export const App = () => {
     return (
         <div className='app'>
             <Router>
-                {content}
+                <Header />
+                    {content}
+                <Footer />
             </Router>
         </div>
     )
--- src/content/pages/SignIn.jsx
+++ src/content/pages/SignIn.jsx
@@ -1,26 +1,86 @@
 // dependencies
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Redirect } from 'react-router-dom';
 // fontawesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserShield } from '@fortawesome/free-solid-svg-icons';
 
 export const SignIn = props => {
+    console.log('Yay')
+    const [pigeon, setPigeon] = useState('');
+    const [pigeon_cage_key, setPigeonCageKey] = useState('');
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        setMessage('');
+    }, [pigeon, pigeon_cage_key])
+
+    const login = (e) => {
+        console.log('login')
+        e.preventDefault();
+        fetch(`${process.env.REACT_APP_SERVER_URL}auth/login`, {
+            method: 'POST',
+            body: JSON.stringify({
+                pigeon,
+                pigeon_cage_key
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+            console.log(response);
+            response.json().then(result => {
+                if (response.ok) {
+                    props.updateUser(result.token);
+                }
+                else {
+                    setMessage(`${response.status} ${response.statusText}: ${result.message}`);
+                }
+            })
+            .catch(err => {
+                console.log('Parse JSON failed');
+                setMessage('Error: Pigeon is mightily confused');
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            setMessage('Error: Pigeon is afraid');
+        })
+    }
+
+    if (props.user && props.user.name) {
+        console.log('Hello')
+        return <Redirect to="/profile" />
+    }
 
     return (
         <div className='sign-in'>
             <div className='auth-header'></div>
             <div className='auth-main'>
-                <FontAwesomeIcon className='logo-shield' icon={faUserShield} size='10x' />
-                <h1 className='logo-text'>SquireSpace</h1>
-                <label className='auth-input'>
-                    <h4>Username: </h4>
-                    <input className='auth-input-field' type='text' name='username' />
-                </label>
-                <label className='auth-input'>
-                    <h4>Password: </h4>
-                    <input className='auth-input-field auth-password' type='password' name='password' />
-                </label>
-                <button className='cta-btn'>Sign In</button>
+                <form onSubmit={login}>
+                    <FontAwesomeIcon className='logo-shield' icon={faUserShield} size='10x' />
+                    <h1 className='logo-text'>SquireSpace</h1>
+                    <p className="danger">{message}</p>
+                    <label className='auth-input'>
+                        <h4>Pigeon Name: </h4>
+                        <input 
+                            className='auth-input-field' 
+                            value={pigeon}
+                            onChange={e => setPigeon(e.target.value)} 
+                        />
+                    </label>
+                    <label className='auth-input'>
+                        <h4>Pigeon Cage Key: </h4>
+                        <input 
+                            className='auth-input-field auth-password' 
+                            type='password' 
+                            value={pigeon_cage_key}
+                            onChange={e => setPigeonCageKey(e.target.value)} 
+                        />
+                    </label>
+                    <button className='cta-btn'>Sign In</button>
+                </form>
             </div>
             <div className='auth-footer'></div>
         </div>
